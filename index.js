bfd305ab6ff1cf6e27357de6bab83408e1d2005e
Ben Lmsc <exanova.labs@icloud.com>
2021-08-01 20:54:02 +0200

    feat(plugin): avoids common pitfalls
    add files to package.json + edit manifest (use required parameter and change name) + manage
    status/logs by using utils methods provided by Netlify

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 module.exports = {
-  onPreBuild: ({ inputs }) => {
-    const {prefix, def} = inputs;
+  onPreBuild: ({ inputs, utils }) => {
+    try {
+      const {prefix, def} = inputs;
 
-    if (!def || !def.length) {
-      throw new Error("netlify-plugin-use-env-in-runtime: def input is missing or does not match the required format");
+      const definitions = Array.isArray(def) ? def : def.split(/\s*[;|,|\s]\s*/);
+
+      for (const definition of definitions) {
+        // Use old concat to provide a support to old Node versions
+        const key        = prefix ? (prefix + "_" + definition) : definition;
+        const value      = process.env[definition];
+        process.env[key] = value;
+      }
+
+      utils.status.show({summary: "The environment variables have been added successfully!"});
     }
-
-    const definitions = Array.isArray(def) ? def : def.split(/\s*[;|,|\s]\s*/);
-
-    for (const definition of definitions) {
-      // Use old concat to provide a support to old Node versions
-      const key = prefix ? (prefix + "_" + definition) : definition;
-      const value = process.env[definition];
-      process.env[key] = value;
+    catch (error) {
+      utils.build.failPlugin("The plugin failed to add the environment variables present in the netlify.toml file. Please check your configuration. Stack trace:", {error});
     }
   }
-}
-
-/**
- * Display an error message on the console
- * @param {string} message
- */
-function errorMessage(message) {
-  console.error(message);
-}
+};
