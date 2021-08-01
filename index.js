af008dea33b5d489e04e36ff10945996033b5aa7
Ben Lmsc <exanova.labs@icloud.com>
2021-08-01 12:21:08 +0200

    fix(index.js): change def statement and returns

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ module.exports = {
   onPreBuild: ({ inputs }) => {
     const {prefix, def} = inputs;
 
-    if (!def && !def.length) return;
+    if (!def || !def.length) {
+      throw new Error("netlify-plugin-use-env-in-runtime: def input is missing or does not match the required format");
+    }
 
     const definitions = Array.isArray(def) ? def : def.split(/\s*[;|,|\s]\s*/);
 
     for (const definition of definitions) {
-      const key = prefix ? `${prefix}_${definition}` : definition;
+      // Use old concat to provide a support to old Node versions
+      const key = prefix ? (prefix + "_" + definition) : definition;
       const value = process.env[definition];
       process.env[key] = value;
     }
   }
+}
+
+/**
+ * Display an error message on the console
+ * @param {string} message
+ */
+function errorMessage(message) {
+  console.error(message);
 }
