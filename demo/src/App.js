03bc00b6fc0aa967606305214938ed59e5e58f4b
Ben Lmsc <exanova.labs@icloud.com>
2021-08-01 20:06:02 +0200

    chore(demo): add link to production version

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,14 +1,14 @@
-import logo        from './logo.svg';
-import './App.css';
+import logo        from "./logo.svg";
+import "./App.css";
 import {useEffect} from "react";
 
 function App() {
   useEffect(() => {
-    console.info("The env variables provided:")
+    console.info("The env variables provided:");
     console.table({
-      REACT_APP_UI_TEST: process.env.REACT_APP_UI_TEST,
-      REACT_APP_TOML_ALL_TEST: process.env.REACT_APP_TOML_ALL_TEST,
-      REACT_APP_TOML_PROD_TEST: process.env.REACT_APP_TOML_PROD_TEST,
+      REACT_APP_UI_TEST         : process.env.REACT_APP_UI_TEST,
+      REACT_APP_TOML_ALL_TEST   : process.env.REACT_APP_TOML_ALL_TEST,
+      REACT_APP_TOML_PROD_TEST  : process.env.REACT_APP_TOML_PROD_TEST,
       REACT_APP_TOML_BRANCH_TEST: process.env.REACT_APP_TOML_BRANCH_TEST,
     });
   }, []);
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <h1>
           Netlify plugin
         </h1>
@@ -26,13 +26,24 @@ function App() {
         </p>
         <br/>
         <ol style={{textAlign: "justify", fontSize: "1rem"}}>
-          <li>Environment variable (named <i>REACT_APP_UI_TEST</i>) set in Netlify UI: <b>{process.env.REACT_APP_UI_TEST}</b></li>
-          <li>Environment variable (named <i>REACT_APP_TOML_ALL_TEST</i>) set in Netlify TOML: <b>{process.env.REACT_APP_TOML_ALL_TEST}</b></li>
-          <li>Environment variable (named <i>REACT_APP_TOML_PROD_TEST</i>) set for production in Netlify TOML: <b>{process.env.REACT_APP_TOML_PROD_TEST}</b></li>
-          <li>Environment variable (named <i>REACT_APP_TOML_BRANCH_TEST</i>) set for a branch (check-deploy-branch) in Netlify TOML: <b>{process.env.REACT_APP_TOML_BRANCH_TEST}</b></li>
+          <li>Environment variable (named <i>REACT_APP_UI_TEST</i>) set in Netlify
+            UI: <b>{process.env.REACT_APP_UI_TEST}</b></li>
+          <li>Environment variable (named <i>REACT_APP_TOML_ALL_TEST</i>) set in Netlify
+            TOML: <b>{process.env.REACT_APP_TOML_ALL_TEST}</b></li>
+          <li>Environment variable (named <i>REACT_APP_TOML_PROD_TEST</i>) set for production in Netlify
+            TOML: <b>{process.env.REACT_APP_TOML_PROD_TEST}</b></li>
+          <li>Environment variable (named <i>REACT_APP_TOML_BRANCH_TEST</i>) set for a branch (demo-check-deploy-branch)
+            in Netlify TOML: <b>{process.env.REACT_APP_TOML_BRANCH_TEST}</b></li>
         </ol>
         <br/>
         <code>Check your web console for more information.</code>
+        <br/>
+        <code style={{fontSize: "1rem"}}>
+          Check the production version: <a className="App-link"
+                                                 href="https://use-env-in-runtime-plugin-demo.netlify.app"
+                                                 target="_blank"
+                                                 rel="noopener noreferrer">click on me!</a> 👺
+        </code>
         <br/>
         <a
           className="App-link"
